fbef531715de2b31b8b5fbd45e9bfd0ab5de6019
Brian Archibald <brianallenarchibald@gmail.com>
2021-04-18 16:00:37 -0500

    Nested for loops, check all smaller nums in array

--- lc.py
+++ lc.py
@@ -33,3 +33,30 @@ class Solution:
 
 # we are adding 1 to city_a which doesnt really matter because we only care about city_b
 # and then return the city with 0 since it only shows up once
+
+#########################################################################
+
+# 1365. How Many Numbers Are Smaller Than the Current Number
+# Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+# Return the answer in an array.
+
+# Example 1:
+
+# Input: nums = [8,1,2,2,3]
+# Output: [4,0,1,1,3]
+# Explanation:
+# For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        output = []
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                print('i=', i, 'j=', j)
+                print('nums[i]=', nums[i], 'nums[j]=', nums[j])
+                if j != i and nums[j] < nums[i]:
+                    count += 1
+            output.append(count)
+        return output
