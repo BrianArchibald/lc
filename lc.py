a4bf5d8bc9d8233b7a922cb2c43e49c5bd263a5a
Brian Archibald <brianallenarchibald@gmail.com>
2021-04-20 17:04:27 -0500

    len of palidrome in string

--- lc.py
+++ lc.py
@@ -140,3 +140,61 @@ class Solution:
                 
         return -1
 
+########################################################################
+
+# 409. Longest Palindrome
+# Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+# Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+# Example 1:
+
+# Input: s = "abccccdd"
+# Output: 7
+# Explanation:
+# One longest palindrome that can be built is "dccaccd", whose length is 7.
+# Example 2:
+
+# Input: s = "a"
+# Output: 1
+# Example 3:
+
+# Input: s = "bb"
+# Output: 2
+ 
+# Constraints:
+
+# 1 <= s.length <= 2000
+# s consists of lowercase and/or uppercase English letters only.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+            print('1st', char, count[char])
+        
+        result = 0
+        is_odd = False
+        for char in count:
+            if count[char] % 2 == 0:
+                print('2nd', char, count[char])
+                result += count[char]
+            else:
+                is_odd = True
+        
+        if is_odd:
+            result += 1
+         
+        return result
+
+# 1st a 1
+# 1st b 1
+# 1st c 1
+# 1st c 2
+# 1st c 3
+# 1st c 4
+# 1st d 1
+# 1st d 2
+# 2nd c 4
+# 2nd d 2
