b48493a81bdebd8ee86618c82f224f36423a41b1
Brian Archibald <brianallenarchibald@gmail.com>
2021-04-30 15:41:41 -0500

    Buy and Sell Stock

--- lc.py
+++ lc.py
@@ -377,3 +377,61 @@ This is a better optimized solution:
                 return [d[m], i]
             else:
                 d[n] = i
+
+
+121. Best Time to Buy and Sell Stock
+Easy
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minPurchase = prices[0]
+        for i in range(1, len(prices)):		
+            print(prices[i], 'i')
+            maxProfit = max(maxProfit, prices[i] - minPurchase)
+            print(maxProfit, 'maxprofit')
+            minPurchase = min(minPurchase, prices[i])
+            print(minPurchase, 'minpur')
+        return maxProfit
+
+
+1 i
+0 maxprofit
+1 minpur
+5 i
+4 maxprofit
+1 minpur
+3 i
+4 maxprofit
+1 minpur
+6 i
+5 maxprofit
+1 minpur
+4 i
+5 maxprofit
+1 minpur
+####################################################################################:w
