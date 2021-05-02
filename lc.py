a51107bdefea56170cba66961a1108ebf17fcb78
Brian Archibald <brianallenarchibald@gmail.com>
2021-05-02 12:54:36 -0500

    quicksort

--- lc.py
+++ lc.py
@@ -463,3 +463,37 @@ class Solution:
         if len(dup) != len(nums):
             return True
         return False
+
+######################################################################################
+Quicksort example
+
+def quicksort(array):
+    if array < 2:
+        return array
+
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] = quicksort(greater)
+
+##########################################################################################
+
+238. Product of Array Except Self
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+
+
