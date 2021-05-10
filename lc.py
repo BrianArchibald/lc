9f09ba01baac343197372c714fd50c1a6dde2883
Brian Archibald <brianallenarchibald@gmail.com>
2021-05-09 20:53:00 -0500

    solution for 3sum On^2

--- lc.py
+++ lc.py
@@ -734,4 +734,41 @@ Constraints:
 0 <= nums.length <= 3000
 -105 <= nums[i] <= 105
 
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        
+        triplets = []
+        
+        for i in range(len(nums) - 2):
+            
+            # this is for non distinct 
+            if i > 0 and nums[i] == nums[i -1]: continue
 
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                
+                current_sum = nums[i] + nums[left] + nums[right]
+                
+                if current_sum == 0:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    
+                    
+                    # next two are for non distinct too
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+                
+                elif current_sum < 0:
+                    left += 1
+                
+                else:
+                    right -= 1
+        return triplets
