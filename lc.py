5341790fedc6a15f0fe15db43eba724e113793d3
Brian Archibald <brianallenarchibald@gmail.com>
2021-06-13 15:09:52 -0500

    longest increasing substring answer

--- lc.py
+++ lc.py
@@ -1696,3 +1696,18 @@ Constraints:
 -104 <= nums[i] <= 104
  
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+                    
+        return max(dp)
