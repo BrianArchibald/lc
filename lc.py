ddf89e12dc3295ed98cc7b07dec7755b15c74fb9
Brian Archibald <brianallenarchibald@gmail.com>
2021-06-19 11:25:25 -0500

    combination sum - dp

--- lc.py
+++ lc.py
@@ -1758,3 +1758,57 @@ class Solution:
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
         return dp[len(s)]
+#####################################################################################################################
+377. Combination Sum IV   
+
+DP ---
+Also, just to take this chance to review some high level rules for dp. DP algorithm is best for:
+
+Min/Max questions
+True/False questions
+Number of ways questions
+
+
+Medium
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+ 
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+All the elements of nums are unique.
+1 <= target <= 1000
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for t in range(1, target + 1):
+            for n in nums:
+                if n <= t:
+                    dp[t] += dp[t - n]
+                    
+        return dp[target]
+
