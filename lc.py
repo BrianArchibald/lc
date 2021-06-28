da02c2b5dcd6bfe58b7adf0951b35c9a39f75e9f
Brian Archibald <brianallenarchibald@gmail.com>
2021-06-28 17:31:58 -0500

    jump game

--- lc.py
+++ lc.py
@@ -2045,4 +2045,39 @@ class Solution:
         return dp[n-1][m-1]
 
 i###########################################################################################################################
+55. Jump Game
+Medium
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_jump, N, i = 0, len(nums) -1, 0
+        
+        while i <= max_jump and i < N:
+            if max_jump >= N:
+                break
+            max_jump = max(max_jump, i+nums[i])
+            i += 1
+            
+        return max_jump >= N
 
