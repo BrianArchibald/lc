23cf7d484d18e302d9f80b3f3eb2602ce6020012
Brian Archibald <brianallenarchibald@gmail.com>
2021-08-07 14:03:21 -0500

    2 sums

--- lc.py
+++ lc.py
@@ -409,6 +409,44 @@ This is a better optimized solution:
             else:
                 d[n] = i
 
+# clearer
+    def twoSum(self, nums, target):
+       seen = {}
+       for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in seen: #3
+               return [i, seen[remaining]]  #4
+           else:
+               seen[value] = i  #5
+
+###################################################################################################33333
+167. Two Sum II - Input array is sorted
+
+Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, v in enumerate(numbers):
+            remaining = target - numbers[i]
+            if remaining in seen:
+                return [seen[remaining] + 1, i + 1] 
+            else:
+                seen[v] = i 
+
+###############################################################################################################
+
 
 121. Best Time to Buy and Sell Stock
 Easy
