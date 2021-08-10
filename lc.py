7fc479fdbd9c377c0ed280a00fcca0c5fc62a0cd
Brian Archibald <brianallenarchibald@gmail.com>
2021-08-10 16:34:58 -0500

    more

--- lc.py
+++ lc.py
@@ -552,6 +552,14 @@ class Solution:
             print(minPurchase, 'minpur')
         return maxProfit
 
+def maxProfit(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
 
 1 i
 0 maxprofit
