51fc701e856de4b3dedcd3bdb4afef66524aa671
Brian Archibald <brianallenarchibald@gmail.com>
2021-09-20 14:54:28 -0500

    binary tree

--- lc.py
+++ lc.py
@@ -2493,3 +2493,34 @@ class Solution:
         dfs(root)
 
         return root
+
+i#####################################################################################################
+
+# binary tree,traversal, return a list... Medium
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = defaultdict(list)
+        
+        def dfs(node, level):
+            if not node: return
+            
+            d[level].append(node.val)
+            dfs(node.left, level+1)
+            dfs(node.right, level+1)
+            
+        dfs(root, 0)
+        #  we dont need the level output, so just return the vals
+        return d.values()
