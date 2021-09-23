aabc840e14071b15315559a5779cb2299e41a26e
BrianArchibald <brianallenarchibald@gmail.com>
2021-09-23 10:36:56 -0500

    construct binary tree

--- lc.py
+++ lc.py
@@ -2566,3 +2566,23 @@ class Solution:
         # -----------------------------------------------------------
         return bool(s and t) and (is_the_same(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t) )
 
+        i######################################################################################################################
+
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Medium
+
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
