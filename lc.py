d280f0c26443eae679a493ace7d1280fe01479ef
Brian Archibald <brianallenarchibald@gmail.com>
2021-09-29 13:09:44 -0500

    binary tree recusion

--- lc.py
+++ lc.py
@@ -2642,4 +2642,37 @@ class Solution:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
 
+###########################################################################################
 
+# Binary tree post order
+145. Binary Tree Postorder Traversal
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+
+############################################################################################################
+
+94. Binary Tree Inorder Traversal
+Easy
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
