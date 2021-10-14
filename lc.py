7d3585cb263d5d3255f668384309bd602b2e7368
BrianArchibald <brianallenarchibald@gmail.com>
2021-10-14 08:55:39 -0500

    kth smallest element in bst

--- lc.py
+++ lc.py
@@ -2816,3 +2816,20 @@ class Solution:
                 return dfs(lower,node.val,node.left) and dfs(node.val, upper, node.right)
         
         return dfs(float('-inf'),float('inf'),root)
+
+###########################################################################################################################
+
+230. Kth Smallest Element in a BST
+Medium
+
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+
