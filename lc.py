06ff1c39fad5827aa10dada3136842b0e3da3a83
Brian Archibald <brianallenarchibald@gmail.com>
2021-11-24 18:52:12 -0600

    num of islands

--- lc.py
+++ lc.py
@@ -3120,6 +3120,38 @@ Output: [[0,0],[0,1],[1,0],[1,1]]
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pass
+    #  will come back to this
 
 
+
+####################################################################################################################
+
+200. Number of Islands
+Medium
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
 
