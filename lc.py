7cd731f9e4ba71c6d4edc5c131f17369b4b8a8d4
Brian Archibald <brianallenarchibald@gmail.com>
2021-11-24 19:08:16 -0600

    num of islands

--- lc.py
+++ lc.py
@@ -3155,3 +3155,20 @@ Output: 3
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
+        def dfs(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] == '0':
+                return
+
+            grid[i][j] = '0'
+            dfs(grid, i - 1, j)  # up
+            dfs(grid, i + 1, j)  # down
+            dfs(grid, i, j - 1)  # left
+            dfs(grid, i, j + 1)  # right
+
+        res = 0
+        for i, iv in enumerate(grid):
+            for j, jv in enumerate(grid[i]):
+                if grid[i][j] == '1':
+                    res += 1
+                    dfs(grid, i, j)
+        return res
